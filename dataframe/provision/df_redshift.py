c1b060d10c820745f10d915d7ffefd601a759202
mfchandra2007 <mfchandra2007@gmail.com>
2021-03-11 16:09:03 +0530

    updates for uutil

--- dataframe/provision/df_redshift.py
+++ dataframe/provision/df_redshift.py
@@ -2,7 +2,8 @@ from pyspark.sql import SparkSession
 import yaml
 import os.path
 import sys
-import aws_utils as ut
+# import aws_utils as ut
+import utils1.aws_utils as ut
 
 
 if __name__ == '__main__':
